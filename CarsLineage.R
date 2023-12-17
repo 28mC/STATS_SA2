43bfaeabe067aea0fcbfeee9970df1ee5d99906e
28mC <97728283+28mC@users.noreply.github.com>
2023-12-17 16:43:28 +0800

    Add files via upload

---
+++ CarsLineage.R
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+from statsmodels.stats.anova import AnovaRM
+
+#Data
+dataframe = pd.DataFrame({'Cars': np.repeat([1, 2, 3, 4, 5], 4),
+  'Oil': np.tile([1, 2, 3, 4], 5),
+  'Mileage': [36, 38, 30, 29,
+              34, 38, 30, 29,
+              34, 28, 38, 32,
+              38, 34, 20, 44,
+              26, 28, 34, 50]})
+
+#Repeated-measuresANOVA
+result = AnovaRM(data=dataframe, depvar='Mileage', subject='Cars', within=['Oil']).fit()
+
+#displayANOVAtable
+print(result)
